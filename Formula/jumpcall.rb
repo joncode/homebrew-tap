e74e65b9d86ef15e7fd680fc26c12ac242d97bfd
Jon Anders <programcodex@gmail.com>
2026-08-05 13:19:56 -0400

    jumpcall 0.2.0

---
+++ Formula/jumpcall.rb
@@ -0,0 +1,36 @@
+class Jumpcall < Formula
+  desc "Jump to your live video call from the macOS menu bar or a hotkey"
+  homepage "https://github.com/joncode/jumpcall"
+  url "https://github.com/joncode/jumpcall/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "3deb04a8aaac668a1e1b9bf7e4dda5c0c7feaf3e8a255bfb450f1e3fe5c3a314"
+  license "MIT"
+  head "https://github.com/joncode/jumpcall.git", branch: "main"
+
+  depends_on macos: :sonoma
+
+  def install
+    system "swift", "build", "-c", "release", "--disable-sandbox"
+    app = prefix/"JumpCall.app"
+    (app/"Contents/MacOS").mkpath
+    cp ".build/release/jumpcall", app/"Contents/MacOS/jumpcall"
+    cp "Resources/Info.plist", app/"Contents/Info.plist"
+    (app/"Contents/PkgInfo").write "APPL????"
+    system "codesign", "--force", "--sign", "-",
+           "--identifier", "io.github.joncode.jumpcall", app
+    bin.install_symlink app/"Contents/MacOS/jumpcall"
+  end
+
+  def caveats
+    <<~EOS
+      To finish setup (copies the app to ~/Applications, enables
+      launch-at-login, and starts the menu-bar icon):
+        jumpcall install
+
+      Re-run "jumpcall install" after every "brew upgrade jumpcall".
+    EOS
+  end
+
+  test do
+    assert_match "jumpcall", shell_output("#{bin}/jumpcall version")
+  end
+end
---
+++ README.md
@@ -0,0 +1,8 @@
+# homebrew-tap
+
+Homebrew formulae for [joncode](https://github.com/joncode)'s tools.
+
+```bash
+brew install joncode/tap/jumpcall
+jumpcall install
+```
